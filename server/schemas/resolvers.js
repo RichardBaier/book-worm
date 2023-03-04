ed5a98a5312d3e20ef52a39a83b879cf01ec138e
SuperDork <bearbaier@gmail.com>
2023-03-04 13:55:22 -0600

    finished mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,24 +14,31 @@ const resolvers = {
         }
     },
     Mutation: {
+        //Allows the user to login and and authenticate themselves
         login: async (parent, { email, password }) => {
-            const user = await User.findOne( { email });
+            const user = await User.findOne({ email });
+      
             if (!user) {
-                throw new AuthenticationError('Incorrect credentials')
+              throw new AuthenticationError('No user found with this email address');
             }
+      
             const correctPw = await user.isCorrectPassword(password);
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials')
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
             }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+        // Creates the user if it doesn't already exist
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
-        },
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-
-            return { token, user };
-        },
+          },
+        // Deletes books
         removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
@@ -42,6 +49,7 @@ const resolvers = {
                 return updatedUser;
             }
         },
+        // Saves the books
         saveBook: async (parent, { book }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
--- server/server.js
+++ server/server.js
@@ -3,11 +3,23 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({app});
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -15,6 +27,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
