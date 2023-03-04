d286e487d1c4987530d1da143254bf398b29f6ed
SuperDork <bearbaier@gmail.com>
2023-03-04 14:11:40 -0600

    stuck

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,41 +1,57 @@
-import decode from 'jwt-decode';
+const jwt = require('jsonwebtoken');
 
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
-  loggedIn() {
-    const token = this.getToken();
-    // If there is a token and it's not expired, return `true`
-    return token && !this.isTokenExpired(token) ? true : false;
-  }
-
-  isTokenExpired(token) {
-    // Decode the token to get its expiration time that was set by the server
-    const decoded = decode(token);
-    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
-    if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem('id_token');
-      return true;
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function ({ req, connection }, res, next) {
+    let token;
+    
+    if (req) {
+      // allows token to be sent via  req.query or headers
+      token = req.query.token || req.headers.authorization;
+  
+      // ["Bearer", "<tokenvalue>"]
+      if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+      }
+    } else if (connection) {
+      // allows token to be sent via connectionParams
+      token = connection.context.Authorization;
     }
-    // If token hasn't passed its expiration time, return `false`
-    return false;
-  }
 
-  getToken() {
-    return localStorage.getItem('id_token');
-  }
+    if (!token) {
+      return res.status(400).json({ message: 'You have no token!' });
+    }
 
-  login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
+    try {
+      // verify token and get user data out of it
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
+    }
 
-  logout() {
-    localStorage.removeItem('id_token');
-    window.location.reload();
-  }
-}
+    // add the user data to the context
+    if (req) {
+      req.context = {
+        user: req.user,
+      };
+    } else if (connection) {
+      connection.context = {
+        user: req.user,
+      };
+    }
 
-export default new AuthService();
+    // send to next endpoint
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
