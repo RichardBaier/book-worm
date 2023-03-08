84b5e2bb61228039a73235bd31e90a31feea316d
SuperDork <bearbaier@gmail.com>
2023-03-08 00:52:21 -0600

    update

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { useMutation } from '@apollo/react-hooks';
+import {  searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -56,7 +58,7 @@ const SearchBooks = () => {
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
+    
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
@@ -66,18 +68,16 @@ const SearchBooks = () => {
       return false;
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
+   
+      const response = await saveBook({variables: {input: bookToSave}});
 
-      if (!response.ok) {
+      if (!response.data) {
         throw new Error('something went wrong!');
       }
 
-      // if book successfully saves to user's account, save book id to state
+      
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
+
   };
 
   return (
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SavedBookInput): User
+        saveBook(book: SavedBookInput!): User
         removeBook(bookId: String!): User
     }
     type User {
@@ -15,9 +15,17 @@ const typeDefs = gql`
         username: String
         email: String
         bookCount: Int
-        savedBooks: [Book]
+        savedBooks: [Book]!
     }
     type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input SavedBookInput {
         bookId: String
         authors: [String]
         description: String
